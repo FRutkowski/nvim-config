febbb9d2a90ff12d8848ff3c0a78b1f608c06912
SiwyKot41 <frutkowski80@gmail.com>
2023-12-01 22:44:00 +0100

    add autopairs

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use 'rafamadriz/friendly-snippets'
+  use 'windwp/nvim-autopairs'
   use {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
