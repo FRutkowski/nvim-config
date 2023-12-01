92ca90087c1b5a96bb030d5e714846cc62fc9d31
SiwyKot41 <frutkowski80@gmail.com>
2023-12-01 14:28:52 +0100

    initial commit

---
+++ lua/core/plugin_config/nvim-tree.lua
@@ -0,0 +1,9 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+--
+--vim.opt.termguicolors = true
+require("nvim-tree").setup()
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-f>', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })
+
+
---
+++ lua/core/plugin_config/telescope.lua
@@ -0,0 +1,6 @@
+local builtin = require('telescope.builtin')
+vim.api.nvim_set_keymap('n', '<c-p>', builtin.find_files, {})
+vim.api.nvim_set_keymap('n', '<Space><Space>', builtin.live_grep, {})
+vim.api.nvim_set_keymap('n', '<Space>fg', builtin.buffers, {})
+vim.api.nvim_set_keymap('n', '<Space>fh', builtin.help_tags, {})
+
