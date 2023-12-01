000b51141627b107aa0d693387196780d7c97b93
SiwyKot41 <frutkowski80@gmail.com>
2023-12-01 17:04:46 +0100

    add lsp, snippets and code completion

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,32 @@
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { 'lua_ls', 'volar', 'tailwindcss' }
+})
+
+local on_attach = function(_, _)
+  vim.api.nvim_set_keymap('n', '<leader>rn', vim.lsp.buf.rename, {})
+  vim.api.nvim_set_keymap('n', '<ALT><ENTER>', vim.lsp.code_action {})
+
+  vim.api.nvim_set_keymap('n', 'gd', vim.lsp.buf.definition, {})
+  vim.api.nvim_set_keymap('n', 'gi', vim.lsp.buf.implementation, {})
+  vim.api.nvim_set_keymap('n', 'gr', require('telescope.builtin').lsp_references, {})
+  vim.api.nvim_set_keymap('n', 'K', vim.lsp.buf.hover, {})
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+require('lspconfig').lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require('lspconfig').volar.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require('lspconfig').tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -18,9 +18,19 @@ return require('packer').startup(function(use)
   use 'nvim-lualine/lualine.nvim'
   use 'folke/tokyonight.nvim'
   use 'nvim-treesitter/nvim-treesitter'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
+  use {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+  }
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
+    tag = '0.1.4',
     requires = {{ 'nvim-lua/plenary.nvim' }}
   }
   -- My plugins here
